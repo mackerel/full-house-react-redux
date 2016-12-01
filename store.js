c9dc59f5caa83d6252cc27921e3c20d1a89ece2f
Matt Burch <mattburch.media@gmail.com>
2016-12-01 08:49:43 +0000

    Added localStorage caching of state

--- store.js
+++ store.js
@@ -1,7 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import dataService from './services/dataService'
 import { reducers } from './reducers'
+import persistState from 'redux-localstorage'
+
 const preloadedState = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(reducers, preloadedState, applyMiddleware(dataService))
+
+const enhancer = compose(
+  applyMiddleware(dataService),
+  persistState()
+)
+
+const store = createStore(reducers, preloadedState, enhancer )
 export default store
